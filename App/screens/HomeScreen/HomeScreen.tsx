4673033ad1e0a2052ad424ef7c0362901ee02ff0
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2025-01-23 14:32:32 +0100

    add navigation

---
+++ App/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text>Hello from Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
